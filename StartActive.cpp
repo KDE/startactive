a0c3c70cada1ecd3c192a1027e7a9939a06c50cb
Ivan Čukić <ivan.cukic@kde.org>
2011-10-21 22:28:51 +0200

    initial commit

---
+++ StartActive.cpp
@@ -0,0 +1,54 @@
+/*
+ *   Copyright (C) 2011 Ivan Cukic <ivan.cukic(at)kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "StartActive.h"
+#include "startactiveadaptor.h"
+
+/**
+ *
+ */
+class StartActive::Private {
+public:
+
+};
+
+StartActive::StartActive(int argc, char ** argv)
+    : QCoreApplication(argc, argv),
+      d(new Private())
+{
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    new StartActiveAdaptor(this);
+    dbus.registerObject("/StartActive", this);
+}
+
+StartActive::~StartActive()
+{
+    delete d;
+}
+
+void StartActive::start()
+{
+
+}
+
+void StartActive::stop()
+{
+
+}
+
