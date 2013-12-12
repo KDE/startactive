d748bda78ce3dd814bc9403189a28e70c288f2b9
Marco Martin <notmart@gmail.com>
2013-12-12 21:29:48 +0100

    don't fiddle with KDEDIRS

--- env.conf.cmake
+++ env.conf.cmake
@@ -1,5 +1,3 @@
-KDEDIR=@KDE_INSTALL_PREFIX@
-KDEDIRS=@KDE_INSTALL_PREFIX@
 
 XCURSOR_PATH=@KDE_INSTALL_PREFIX@/share/icons::~/.icons:/usr/share/icons:/usr/share/pixmaps:/usr/X11R6/lib/X11/icons
 #XCURSOR_THEME=plasmamobilemouse
